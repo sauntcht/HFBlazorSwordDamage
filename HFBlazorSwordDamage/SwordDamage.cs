cddcfffcc5d7b12eb9ec007132511312176e754c
ehtom <ehtom@DESKTOP-SS1Q4KR>
2022-03-15 08:23:05 -0400

    Add project files.

---
+++ HFBlazorSwordDamage/SwordDamage.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+
+namespace HFBlazorSwordDamage
+
+{
+    public class SwordDamage
+    {
+        public SwordDamage(){
+            Debug.WriteLine("Created an instance of sword damage.");
+        }
+        
+        
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
+
+        public int Roll;
+        public decimal MagicMultiplier = 1m;
+        public int FlamingDamage =0;
+        public int Damage;
+
+        public void CalculateDamage()
+        {
+            Damage = (int)(Roll * MagicMultiplier) + BASE_DAMAGE + FlamingDamage;
+        }
+        public void SetMagic(bool isMagic)
+        {
+            if (isMagic)
+            {
+                MagicMultiplier = 1.75m;
+            }
+            else
+            {
+                MagicMultiplier = 1m;
+            }
+            CalculateDamage();
+        }
+
+        public void SetFlaming(bool isFlaming)
+        { 
+            CalculateDamage();
+            if (isFlaming)
+            {
+                Damage += FLAME_DAMAGE;
+            }
+        }
+    }
+}
